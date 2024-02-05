b7f1945d86f1203c00eb58bfac9bb4392483709e
santanubiswas <santanubiswas@yahoo.com>
2024-02-06 00:51:05 +0530

    only card

--- arr_lib/arr_charts.py
+++ arr_lib/arr_charts.py
@@ -6,9 +6,11 @@ from arr_lib.styling import BUTTON_STYLE
 from arr_lib.styling import MARKDOWN_STYLES
 from arr_lib.styling import GLOBAL_STYLING
 
+from arr_lib import styling as style
 
 
 
+@st.cache_data
 def arr_walk_chart(metrics_df, bar_color, chart_title): 
     """
     generate and return an altair chart - for customer ARR walk 
@@ -81,7 +83,7 @@ def arr_walk_chart(metrics_df, bar_color, chart_title):
     return arr_walk_chart
 
 
-
+@st.cache_data
 def cust_count_chart(df, chart_color, chart_title): 
        
     # Check if 'Total_Sales' column exists, and drop it if it does
@@ -116,7 +118,7 @@ def cust_count_chart(df, chart_color, chart_title):
 
     return cust_count_result
 
-
+@st.cache_data
 def cust_count_waterfall_chart(df,  chart_title): 
 
      
@@ -178,7 +180,7 @@ def cust_count_waterfall_chart(df,  chart_title):
     
     return count_wf_chart
 
-
+@st.cache_data
 def top_cust_chart(customer_arr_df, bar_color, chart_title): 
     """
     generate and return an altair chart - for customer pareto 
@@ -241,7 +243,7 @@ def top_cust_chart(customer_arr_df, bar_color, chart_title):
     return top_final_chart
 
 
-
+@st.cache_data
 def cust_arr_waterfall_chart(df,  chart_title): 
 
      
@@ -308,3 +310,384 @@ def cust_arr_waterfall_chart(df,  chart_title):
     )
     
     return arr_wf_chart
+
+
+
+
+
+@st.cache_data
+def create_waterfall_chart(input_df, chart_width):
+    """
+    
+    ARR Waterfall chart 
+    
+    """
+
+    df = input_df.copy()
+
+    min_val = df['curRev'].min()
+    padding = (df['curRev'].max() - min_val) * 0.05  # Adjust padding as needed
+
+    common_y_scale = alt.Scale(domainMin=min_val - padding)
+
+
+    base = alt.Chart(df).encode(
+        x=alt.X('period:N', axis=alt.Axis(labelAngle=0, title=None, domain=True, domainColor='black', domainWidth=1, ticks=True, tickWidth=1, tickSize=8))
+    )
+
+
+    # newBusiness bars
+    nb_bars = base.transform_filter(
+        alt.datum.newBusiness != 0
+    ).mark_bar(size=20, color=style.newBusiness_color).encode(
+        y=alt.Y('nb_end:Q', scale=common_y_scale, axis=alt.Axis(title="Recurring Revenue (in thousands)", format=',.0f', domain=True, domainColor='black', domainWidth=1, ticks=True, tickWidth=1, tickSize=8)),
+        y2='preRev:Q'
+    )
+
+    # newBusiness labels with explicit Y positioning
+    nb_labels = base.transform_filter(
+        alt.datum.newBusiness != 0
+    ).mark_text(angle=270, dx=15).encode(
+        x='period:N',
+        y='nb_end:Q',
+        #y=alt.Y('sales_end:Q', axis=alt.Axis(labels=False)),  # Position based on sales_end
+        text=alt.Text('newBusiness:Q', format=',.0f')
+    )
+
+
+    # upSell bars without offset (ex_offset == 0)
+    upSell_bars_no_offset = base.mark_bar(size=20, color=style.upSell_color, xOffset=0).encode(
+        y='ex_end:Q',
+        y2='nb_end:Q'
+    ).transform_filter(
+        (alt.datum.upSell != 0) & (alt.datum.ex_offset == 10)
+    )
+
+    # upSell bars with offset (ex_offset == 20)
+    upSell_bars_10_offset = base.mark_bar(size=20, color=style.upSell_color, xOffset=20).encode(
+        y='ex_end:Q',
+        y2='nb_end:Q'
+    ).transform_filter(
+        (alt.datum.upSell != 0) & (alt.datum.ex_offset == 20)
+    )
+
+
+
+    # upsell  labels with explicit Y positioning
+    upSell_label_no_offset = base.transform_filter(
+        (alt.datum.upSell != 0) & (alt.datum.ex_offset == 10)
+    ).mark_text(angle=270, dx=15, xOffset=0).encode(
+        x='period:N',
+        y='ex_end:Q',
+        text=alt.Text('upSell:Q', format=',.0f')
+    )
+
+
+    upSell_label_10_offset = base.transform_filter(
+        (alt.datum.upSell != 0) & (alt.datum.ex_offset == 20)
+    ).mark_text(angle=270, dx=15, xOffset=20).encode(
+        x='period:N',
+        y='ex_end:Q',
+        text=alt.Text('upSell:Q', format=',.0f')
+    )
+
+
+
+    # downSell bars without offset (ex_offset == 0)
+    downSell_bars_no_offset = base.mark_bar(size=20, color=style.downSell_color, xOffset=0).encode(
+        y='cnt_end:Q',
+        y2='ex_end:Q'
+    ).transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 10)
+    )
+
+        # downSell bars with offset (ex_offset == 20)
+    downSell_bars_10_offset = base.mark_bar(size=20, color=style.downSell_color, xOffset=20).encode(
+        y='cnt_end:Q',
+        y2='ex_end:Q'
+    ).transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 20)
+    )
+
+    # downSell bars with offset (ex_offset == 20)
+    downSell_bars_20_offset = base.mark_bar(size=20, color=style.downSell_color, xOffset=40).encode(
+        y='cnt_end:Q',
+        y2='ex_end:Q'
+    ).transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 30)
+    )
+
+    # downSell  labels with explicit Y positioning
+    downSell_label_no_offset = base.transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 10)
+    ).mark_text(angle=270, dx=15, xOffset=0).encode(
+        x='period:N',
+        y='ex_end:Q',
+        text=alt.Text('downSell:Q', format=',.0f')
+    )
+
+
+    downSell_label_10_offset = base.transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 20)
+    ).mark_text(angle=270, dx=15, xOffset=20).encode(
+        x='period:N',
+        y='ex_end:Q',
+        text=alt.Text('downSell:Q', format=',.0f')
+    )
+
+
+    downSell_label_20_offset = base.transform_filter(
+        (alt.datum.downSell != 0) & (alt.datum.cnt_offset == 30)
+    ).mark_text(angle=270, dx=15, xOffset=40).encode(
+        x='period:N',
+        y='ex_end:Q',
+        text=alt.Text('downSell:Q', format=',.0f')
+    )
+
+
+    # churn bars without offset (ex_offset == 0)
+    churn_bars_no_offset = base.mark_bar(size=20, color=style.churn_color, xOffset=0).encode(
+        y='churn_end:Q',
+        y2='cnt_end:Q'
+    ).transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 10)
+    )
+
+        # churn bars with offset (ex_offset == 20)
+    churn_bars_10_offset = base.mark_bar(size=20, color=style.churn_color, xOffset=20).encode(
+        y='churn_end:Q',
+        y2='cnt_end:Q'
+    ).transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 20)
+    )
+
+    # churn bars with offset (ex_offset == 20)
+    churn_bars_20_offset = base.mark_bar(size=20, color=style.churn_color, xOffset=40).encode(
+        y='churn_end:Q',
+        y2='cnt_end:Q'
+    ).transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 30)
+    )
+
+    # churn bars with offset (ex_offset == 40)
+    churn_bars_30_offset = base.mark_bar(size=20, color=style.churn_color, xOffset=60).encode(
+        y='churn_end:Q',
+        y2='cnt_end:Q'
+    ).transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 40)
+    )
+
+
+    # churn  labels with explicit Y positioning
+    churn_label_no_offset = base.transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 10)
+    ).mark_text(angle=270, dx=15, xOffset=0).encode(
+        x='period:N',
+        y='cnt_end:Q',
+        text=alt.Text('churn:Q', format=',.0f')
+    )
+
+    churn_label_10_offset = base.transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 20)
+    ).mark_text(angle=270, dx=15, xOffset=20).encode(
+        x='period:N',
+        y='cnt_end:Q',
+        text=alt.Text('churn:Q', format=',.0f')
+    )
+
+    churn_label_20_offset = base.transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 30)
+    ).mark_text(angle=270, dx=15, xOffset=40).encode(
+        x='period:N',
+        y='cnt_end:Q',
+        text=alt.Text('churn:Q', format=',.0f')
+    )
+
+
+    churn_label_30_offset = base.transform_filter(
+        (alt.datum.churn != 0) & (alt.datum.churn_offset == 40)
+    ).mark_text(angle=270, dx=15, xOffset=60).encode(
+        x='period:N',
+        y='cnt_end:Q',
+        text=alt.Text('churn:Q', format=',.0f')
+    )
+
+
+    # Rules without offset (ex_offset == 0)
+    rules_no_offset = base.mark_rule(color="#707070", opacity=1, strokeWidth=1, xOffset=-10, x2Offset=10).encode(
+        x='period:N',
+        x2='lead_period:N',
+        y='curRev:Q',
+        y2='curRev:Q'
+    ).transform_filter(
+        (alt.datum.churn_offset == 10)
+    )
+
+    # rules bars with offset (ex_offset == 20)
+    rules_10_offset = base.mark_rule(color="#707070", opacity=1, strokeWidth=1, xOffset=10, x2Offset=10).encode(
+        x='period:N',
+        x2='lead_period:N',
+        y='curRev:Q',
+        y2='curRev:Q'
+    ).transform_filter(
+        (alt.datum.churn_offset == 20)
+    )
+
+    # rules bars with offset (ex_offset == 20)
+    rules_20_offset = base.mark_rule(color="#707070", opacity=1, strokeWidth=1, xOffset=30, x2Offset=10).encode(
+        x='period:N',
+        x2='lead_period:N',
+        y='curRev:Q',
+        y2='curRev:Q'
+    ).transform_filter(
+       (alt.datum.churn_offset == 30)
+    )
+
+    # rules bars with offset (ex_offset == 20)
+    rules_30_offset = base.mark_rule(color="#707070", opacity=1, strokeWidth=1, xOffset=50, x2Offset=10).encode(
+        x='period:N',
+        x2='lead_period:N',
+        y='curRev:Q',
+        y2='curRev:Q'
+    ).transform_filter(
+        (alt.datum.churn_offset == 40)
+    )
+
+    # rules bars with offset (ex_offset == 20)
+    rules_left_offset = base.mark_rule(color="#707070", opacity=1, strokeWidth=1, xOffset=-30, x2Offset=10).encode(
+        x='period:N',
+        x2='lead_period:N',
+        y='curRev:Q',
+        y2='curRev:Q'
+    ).transform_filter(
+        (alt.datum.churn_offset == 0)
+    )
+
+
+    # Text for increase and decrease
+    text_increase = base.transform_filter(
+        (alt.datum.downSell == 0) & (alt.datum.churn == 0) & (alt.datum.period != 'End Period')
+    ).mark_text(dx=50, dy=-10, angle=0, fontWeight='bold', fontSize = 14, align='center').encode(
+        x='period:N',
+        y='curRev:Q',
+        text=alt.Text('curRev:Q', format=',.0f')
+    )
+    text_decrease = base.transform_filter(
+        ((alt.datum.downSell != 0) | (alt.datum.churn != 0)) & (alt.datum.period != 'End Period')
+    ).mark_text(dx=50, dy=10, angle=0, fontWeight='bold', fontSize = 14, align='center').encode(
+        x='period:N',
+        y='curRev:Q',
+        text=alt.Text('curRev:Q', format=',.0f')
+    )
+
+
+
+    # Opening sales bar with xOffset
+    first_period_data = df.iloc[0]
+    opening_rev = alt.Chart(pd.DataFrame({'period': [first_period_data['period']], 
+                                            'preRev': [first_period_data['preRev']], 'startFrom' : [(min_val - padding)],
+                                            })).mark_bar(size=20, color=style.beginPeriod_color, xOffset=-20).encode(
+        x='period:N',
+        y=alt.Y('preRev:Q', scale=common_y_scale), 
+        y2='startFrom:N'
+    )
+
+
+
+    # Add a text label to the opening revenue bar
+    opening_label = alt.Chart(pd.DataFrame({'period': [first_period_data['period']], 
+                                        'startFrom' : [(min_val - padding)],   
+                                        'preRev': [first_period_data['preRev']],
+                                        'preMid': [first_period_data['preMid']],
+                                        'curMid': [first_period_data['curMid']],
+                                        'label': [first_period_data['preRev']]})).mark_text(
+        angle=270,  # Rotate the text by 270 degrees
+        # fontWeight='bold',
+        fontSize = 12,
+        align='center', 
+        baseline='middle',
+        dy = -20  # Adjust this value to position the text within the bar
+    ).encode(
+        x='period:N',
+        y='preMid:Q',
+        y2='startFrom:Q',
+        text=alt.Text('label:N', format=',.0f')  # Assuming you want to display the revenue as text
+    )
+
+    closing_rev = alt.Chart(df[df['period'] == 'End Period']).mark_bar(size=20, color=style.endPeriod_color, xOffset=20).encode(
+        x='period:N',
+        y=alt.Y('curRev:Q', scale=common_y_scale),
+        y2='startFromY'
+    )
+
+        
+    # Add a text label to the opening revenue bar
+    closng_label = alt.Chart(df[df['period'] == 'End Period']).mark_text(
+        angle=270,  # Rotate the text by 270 degrees
+        # fontWeight='bold',
+        fontSize = 12,
+        align='center', 
+        baseline='middle',
+        dy = 20  # Adjust this value to position the text within the bar
+    ).encode(
+        x='period:N',
+        y=alt.Y('curMid:Q', scale=common_y_scale),
+        text=alt.Text('curRev:N', format=',.0f')  # Assuming you want to display the revenue as text
+    )
+
+
+
+
+    main_chart = nb_bars +  nb_labels + \
+                upSell_bars_no_offset + upSell_label_no_offset + upSell_bars_10_offset + upSell_label_10_offset + \
+                downSell_bars_no_offset + downSell_bars_10_offset + downSell_bars_20_offset + \
+                downSell_label_no_offset + downSell_label_10_offset + downSell_label_20_offset + \
+                churn_bars_no_offset +  churn_bars_10_offset + churn_bars_20_offset +  churn_bars_30_offset + \
+                churn_label_no_offset + churn_label_10_offset + churn_label_20_offset + churn_label_30_offset + \
+                rules_no_offset +  rules_10_offset + rules_20_offset +  rules_30_offset + rules_left_offset + \
+                text_increase + text_decrease + opening_rev  +  closing_rev #+ opening_label + closng_label
+    
+                # churn_bars + rules
+
+
+    main_chart = main_chart.properties(width=chart_width, height=500)
+
+    # Update the legend data
+    legend_data = pd.DataFrame({    
+        'category': ['Opening Revenue', 'New Business', 'Expansion', 'Contraction', 'Churn', 'Closing Revenue'],
+        'color': [style.beginPeriod_color, style.newBusiness_color, style.upSell_color , style.downSell_color, style.churn_color, style.endPeriod_color],
+        'x_pos': [1, 2, 3, 4, 5, 6]
+    })
+
+
+
+    # Create colored squares for the legend
+    squares = alt.Chart(legend_data).mark_square(size=250, stroke='gray', strokeWidth=1).encode(
+        x='x_pos:O',
+        color=alt.Color('color:N', scale=None)
+    )
+
+    # Create text labels for the legend with added spacing
+    text = alt.Chart(legend_data).mark_text(align='left', dx=15, fontSize = 12).encode(
+        x=alt.X('x_pos:O', axis=alt.Axis(title=None, labels=False)),
+        text='category:N'
+    )
+
+    # Combine squares and text into one legend chart
+    legend = alt.layer(squares, text).properties(
+        width=chart_width * 0.8,
+        height=50
+    )
+
+   # Combine the main chart with the legend
+    final_chart = alt.vconcat(
+        main_chart,
+        legend,
+        spacing=10,
+        padding= {"bottom": 15, "top":25, "left": 15, "right": 15}
+    ).configure(    
+        background="#F5F8F8" # Set the background color
+    )
+
+    return final_chart
+
