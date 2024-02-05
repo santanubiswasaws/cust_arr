13efc4afd3cdf6141ab781876f74d3c9a488b3c5
santanubiswas <santanubiswas@yahoo.com>
2024-02-05 13:42:06 +0530

    out arr walk - in cust count

--- pages/4_Waterfall_Visualization.py
+++ pages/4_Waterfall_Visualization.py
@@ -187,19 +187,61 @@ st.markdown("<br>", unsafe_allow_html=True)
 ##
 ## ARR Analytics 
 ##
-st.subheader('ARR Trends')
-arr_tab1, arr_tab2= st.tabs(["Final Adjusted ARR", "Uploaded ARR"])
-with arr_tab1: 
+# st.subheader('ARR Trends')
+# arr_tab1, arr_tab2= st.tabs(["Final Adjusted ARR", "Uploaded ARR"])
+# with arr_tab1: 
+
+#     st.markdown("<br>", unsafe_allow_html=True)
+#     arr_result = ac.arr_walk_chart(replan_metrics_df, '#88b988', 'Final Adjusted ARR')
+#     st.altair_chart(arr_result, theme="streamlit", use_container_width=False)
+
+# with arr_tab2: 
+
+#     st.markdown("<br>", unsafe_allow_html=True)
+#     upld_arr_result = ac.arr_walk_chart(metrics_df, '#77aaca', 'Uploaded ARR')
+#     st.altair_chart(upld_arr_result, theme="streamlit", use_container_width=False)
+
+# st.markdown("<br>", unsafe_allow_html=True)
+
+##
+## Countmer count analytics 
+##
+    
+# Check if 'Total_Sales' column exists, and drop it if it does
+# if 'Total_Sales' in replan_logo_metrics_df.columns:
+#     replan_logo_metrics_df = replan_logo_metrics_df.drop(columns=['Total_Sales'])
+
+st.subheader('Customer Counts')
+cust_cout_tab1, cust_cout_tab2= st.tabs(["Final Customer Count", "Uploaded Coustomer Count"])
+
+with cust_cout_tab1: 
 
     st.markdown("<br>", unsafe_allow_html=True)
-    arr_result = ac.arr_walk_chart(replan_metrics_df, '#88b988', 'Final Adjusted ARR')
-    st.altair_chart(arr_result, theme="streamlit", use_container_width=False)
 
-with arr_tab2: 
+    # customer count 
+    replan_cust_count_result = ac.cust_count_chart(replan_logo_metrics_df, '#88b988', 'Adjusted Customer Count' )
+    st.altair_chart(replan_cust_count_result, theme="streamlit", use_container_width=False)
 
     st.markdown("<br>", unsafe_allow_html=True)
-    upld_arr_result = ac.arr_walk_chart(metrics_df, '#77aaca', 'Uploaded ARR')
-    st.altair_chart(upld_arr_result, theme="streamlit", use_container_width=False)
+
+    # customer count waterfall
+    replan_cust_count_wf_result = ac.cust_count_waterfall_chart (replan_logo_metrics_df, 'Adjusted Customer Count Waterfall' )
+    st.altair_chart(replan_cust_count_wf_result, theme="streamlit", use_container_width=False)
+
+
+with cust_cout_tab2: 
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    # customer count 
+    replan_cust_count_result = ac.cust_count_chart(logo_metrics_df, '#77aaca', 'Uploaded Customer Count' )
+    st.altair_chart(replan_cust_count_result, theme="streamlit", use_container_width=False)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    # Customer count waterfall 
+    cust_count_wf_result = ac.cust_count_waterfall_chart (logo_metrics_df, 'Customer Count Waterfall' )
+    st.altair_chart(cust_count_wf_result, theme="streamlit", use_container_width=False)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
